275c26332a7fb5762b7af7330fe8eea5b7fbee47
amilqarkaram <amilqark@gmail.com>
2020-08-02 16:04:43 -0400

    successfully loaded an image stored in database to client

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const bodyParser = require("body-parser");
+const _ = require('lodash');
+const fs = require("fs");
 const ejs = require("ejs");//required to embedd javascript. In other words, dynamically change html by sending data from server and filling in an html template
 const express = require("express");
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 const util = require("util");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
@@ -15,7 +19,8 @@ mongoose.connect("mongodb://localhost:27017/gleema-blogDB", {useNewUrlParser: tr
 const aboutString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at auctor urna nunc id cursus metus aliquam. In metus vulputate eu scelerisque felis. Pulvinar pellentesque habitant morbi tristique senectus et. Fames ac turpis egestas integer eget aliquet nibh praesent. Mattis nunc sed blandit libero volutpat sed cras. Sagittis nisl rhoncus mattis rhoncus. Donec adipiscing tristique risus nec feugiat in fermentum posuere urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at auctor urna nunc id cursus metus aliquam. In metus vulputate eu scelerisque felis. Pulvinar pellentesque habitant morbi tristique senectus et. Fames ac turpis egestas integer eget aliquet nibh praesent. Mattis nunc sed blandit libero volutpat sed cras. Sagittis nisl rhoncus mattis rhoncus. Donec adipiscing tristique risus nec feugiat in fermentum posuere urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at auctor urna nunc id cursus metus aliquam. In metus vulputate eu scelerisque felis. Pulvinar pellentesque habitant morbi tristique senectus et. Fames ac turpis egestas integer eget aliquet nibh praesent. Mattis nunc sed blandit libero volutpat sed cras. Sagittis nisl rhoncus mattis rhoncus. Donec adipiscing tristique risus nec feugiat in fermentum posuere urna.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at auctor urna nunc id cursus metus aliquam. In metus vulputate eu scelerisque felis. Pulvinar pellentesque habitant morbi tristique senectus et. Fames ac turpis egestas integer eget aliquet nibh praesent. Mattis nunc sed blandit libero volutpat sed cras. Sagittis nisl rhoncus mattis rhoncus. Donec adipiscing tristique risus nec feugiat in fermentum posuere urna."
 const blogSchema = new mongoose.Schema({
     postTitle: String,
-    postImage: String,
+    postDate: String,
+    postImage: {data: Buffer, contentType: String},
     postBody: String,
     postType: String
 });
@@ -48,8 +53,13 @@ app.get("/:type",function(req,res){
         if(err){
           console.log(err);
         }
-        else{
-          res.render("post",{imgSrc: "images/Gleema_Generic.jpg",about:aboutString,logginMessage:message,posts:results,postType: req.params.type,accessLevel: accessType});
+        else if(results.length !== 0){
+          let path = "/" + req.params.type;
+        //  console.log(results);
+          img = Buffer.from(results[0].postImage.data,"base64").toString("base64");
+          // let img = "data:" + results[0].postImage.contentType + ";base64," + results[0].postImage.data;
+          // console.log(img);
+          res.render("post",{imgSrc:img,about:aboutString,logginMessage:message,posts:results,postPath: path,accessLevel: accessType});
         }
       });
 }
@@ -57,7 +67,7 @@ else{
   res.render("loginPage");
 }
 });
-app.post("/:type",function(req, res){
+app.post("/:type",upload.single('testImage'),function(req, res){
   // ------------------this part registers users-----------------
   // bcrypt.hash(req.body.password,saltRounds,function(err, hash){
   //   const newAdmin = new Admin({
@@ -103,6 +113,20 @@ else{
 };
 }
 else{
+  console.log(req.file);
+  console.log(req.body);
+  // read the img file from tmp in-memory location
+   var newImg = fs.readFileSync(req.file.path);
+   // encode the file as a base64 string.
+   var encImg = newImg.toString('base64');
+  var newPost = new Posts({
+    postTitle: req.body.postTitle,
+    postDate: req.body.postDate,
+    postBody: req.body.postBody,
+    postImage:{data: Buffer(encImg, 'base64'), contentType: req.file.mimetype},
+    postType: req.params.type
+  });
+  newPost.save();
   // create posts here based on the type
 }
 });
